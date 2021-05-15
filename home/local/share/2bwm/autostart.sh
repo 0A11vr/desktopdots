7be5f56d71abf7cd2adfaa4baecbe4875ae6ec02
PreciseTroll <40143063+PreciseTroll@users.noreply.github.com>
2021-05-15 00:47:15 -0500

    Update autostart.sh

--- home/local/share/2bwm/autostart.sh
+++ home/local/share/2bwm/autostart.sh
@@ -2,24 +2,21 @@
 
 # https://github.com/arcolinux/arcolinux-dwm/blob/master/etc/skel/.config/arco-dwm/autostart.sh
 
-
-
 run() {
 	if ! pgrep "$1"; then
 		"$@"&
 	fi
-}	
+}
 
 run mpd
 run dunst
 run flashfocus
-#run picom --experimental-backend
 run picom
-run mpd-mpris
+(sleep 1s && run mpd-mpris) # needed to properly autostart
+
 
 
 # OLD
 #[ -z "$(pgrep -f dunst)" ] && dunst &
 #[ -z "$(pgrep -f picom)" ] && picom --experimental-backend &
 #[ -z "$(pgrep -f flashfocus)" ] && flashfocus &
-
